9354452205cd824f518f82017a53d605a3211922
Your Name <gms441@mail.ru>
2025-03-19 21:06:14 +0700

    add soundmanager button

--- src/MusicInterface.tsx
+++ src/MusicInterface.tsx
@@ -13,16 +13,20 @@ interface VolumePlayerProps {
 
 export const MusicInterface: FC = () => {
     const [play, setPlay] = useState<boolean>(false);
-    const [volume, setVolume] = useState<number>(0.5);
+
+    const baseVolume: number = +localStorage.getItem('masterVolume') || 0.5;
+    const [volume, setVolume] = useState<number>(baseVolume);
 
     return(
         <div className='iface-group-container'>
             <MusicPlayer play={play} setPlay={setPlay} volume={volume} />
             <VolumeSettings volume={volume} setVolume={setVolume} />
+            <SoundManager />
         </div>
     )
 }
 
+/** Базовый play\pause */
 const MusicPlayer: FC<MusicPlayerProps> = ({ play, setPlay, volume }) => {
     const playerRef: Ref<HTMLAudioElement> = useRef(null);
     const playerHandler = (state: boolean) => {
@@ -55,6 +59,7 @@ const MusicPlayer: FC<MusicPlayerProps> = ({ play, setPlay, volume }) => {
     </div>
 };
 
+/** Громкость всего FIXME */
 const VolumeSettings: FC<VolumePlayerProps> = ({ volume, setVolume }) => {
     const [showSettings, setShowSettings] = useState<boolean>(false);
     const hideVolumeTimer: Ref<number> = useRef(0);
@@ -73,11 +78,17 @@ const VolumeSettings: FC<VolumePlayerProps> = ({ volume, setVolume }) => {
     /** отрисовка всплывающей формы */
     const volumeBtnRef: Ref<HTMLButtonElement> = useRef(null);
     const volumeHoveringFormTop: Ref<number> = useRef(0);
-    const volumeHoveringFormHeight = 31;
     useEffect(() => {
         const volumeBtnCoordinates = volumeBtnRef.current.getBoundingClientRect();
-        volumeHoveringFormTop.current = volumeBtnCoordinates.top - (volumeHoveringFormHeight) / 2;
+        volumeHoveringFormTop.current = volumeBtnCoordinates.top - 15;
     }, [showSettings]);
+
+    /** изменение громкости */
+    const onChangeVolume = (event) => {
+        const volume = +event.target.value;
+        volume > 0 && localStorage.setItem('masterVolume', volume.toString());
+        setVolume(volume);
+    }
 
     /** без звука */
     const oldVolume: Ref<number> = useRef(volume);
@@ -115,8 +126,8 @@ const VolumeSettings: FC<VolumePlayerProps> = ({ volume, setVolume }) => {
             </button>
             { showSettings &&
                 <div
-                    className='hovering-form'
-                    style={{ height: `${volumeHoveringFormHeight}px`, top: volumeHoveringFormTop.current + 'px', padding: '1px 10px' }}
+                    className='hovering-form volume-window'
+                    style={{ top: volumeHoveringFormTop.current + 'px' }}
                     onMouseEnter={() => showHoveringForm(true)}
                     onMouseLeave={() => showHoveringForm(false)}
                 >
@@ -126,9 +137,44 @@ const VolumeSettings: FC<VolumePlayerProps> = ({ volume, setVolume }) => {
                         max="1"
                         step="0.1"
                         value={volume}
-                        onChange={(event) => setVolume(+event.target.value)}
+                        onChange={onChangeVolume}
                     />
                 </div> }
         </div>
     )
-}
+};
+
+/** Выбор звуков */
+const SoundManager: FC = () => {
+    const [managerWindow, showManagerWindow] = useState<boolean>(false);
+
+    const managerBtnRef: Ref<HTMLButtonElement> = useRef(null);
+    const managerHoveringFormTop: Ref<number> = useRef(0);
+    useEffect(() => {
+        const managerBtnCoordinates = managerBtnRef.current.getBoundingClientRect();
+        managerHoveringFormTop.current = managerBtnCoordinates.top - 15;
+    }, [managerWindow]);
+
+    return(
+        <div>
+            <button
+                className='iface-btn'
+                onClick={ () => showManagerWindow(!managerWindow) }
+                ref={managerBtnRef}
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#fff" aria-hidden="true"
+                      data-slot="icon">
+                    <path d="M13.1707057,7 C13.5825421,5.83480763 14.6937812,5 16,5 C17.3062188,5 18.4174579,5.83480763 18.8292943,7 L21,7 C21.5522847,7 22,7.44771525 22,8 C22,8.55228475 21.5522847,9 21,9 L18.8292943,9 C18.4174579,10.1651924 17.3062188,11 16,11 C14.6937812,11 13.5825421,10.1651924 13.1707057,9 L3,9 C2.44771525,9 2,8.55228475 2,8 C2,7.44771525 2.44771525,7 3,7 L13.1707057,7 Z M5.17070571,15 C5.58254212,13.8348076 6.69378117,13 8,13 C9.30621883,13 10.4174579,13.8348076 10.8292943,15 L21,15 C21.5522847,15 22,15.4477153 22,16 C22,16.5522847 21.5522847,17 21,17 L10.8292943,17 C10.4174579,18.1651924 9.30621883,19 8,19 C6.69378117,19 5.58254212,18.1651924 5.17070571,17 L3,17 C2.44771525,17 2,16.5522847 2,16 C2,15.4477153 2.44771525,15 3,15 L5.17070571,15 Z M16,9 C16.5522847,9 17,8.55228475 17,8 C17,7.44771525 16.5522847,7 16,7 C15.4477153,7 15,7.44771525 15,8 C15,8.55228475 15.4477153,9 16,9 Z M8,17 C8.55228475,17 9,16.5522847 9,16 C9,15.4477153 8.55228475,15 8,15 C7.44771525,15 7,15.4477153 7,16 C7,16.5522847 7.44771525,17 8,17 Z"
+                    />
+                </svg>
+            </button>
+            { managerWindow &&
+                <div
+                    className='hovering-form manager-window'
+                    style={{ top: managerHoveringFormTop.current + 'px' }}
+                >
+
+                </div> }
+        </div>
+    )
+};
